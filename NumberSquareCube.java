01cb3abad16ce74f576c0ce75d7677ba4543328d
jahangeer <jahangeerlashari06@gmail.com>
2025-09-13 11:25:59 +0500

    this program print Numbers,Square and Cube without using loop

---
+++ NumberSquareCube.java
@@ -0,0 +1,40 @@
+package Java_Programs;
+
+public class NumberSquareCube {
+
+    public static void main(String[] args) {
+
+        int num = 1; 
+
+        
+
+        System.out.println("Number\tSquare\tCube");
+        System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+        System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+        System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+        System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+         System.out.printf("%d\t%d\t%d%n",num,(num*num),(num*num*num));
+        num++;
+
+        
+    }
+
+   
+
+
+    
+}
