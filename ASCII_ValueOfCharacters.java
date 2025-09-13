14792e8c4855ecb395bc3ca13f4ac40673cde56b
jahangeer <jahangeerlashari06@gmail.com>
2025-09-13 11:49:00 +0500

    Ascii Values of Characters

---
+++ ASCII_ValueOfCharacters.java
@@ -0,0 +1,16 @@
+package Java_Programs;
+
+public class ASCII_ValueOfCharacters {
+
+    public static void main(String[] args) {
+
+        //(int) 'A' cast operator
+        
+
+    System.out.printf("The character %c has the value %d%n", 'A', ((int) 'A'));
+    System.out.printf("The character %c has the value %d%n", 'F', ((int) 'F'));
+    System.out.printf("The character %c has the value %d%n", '*', ((int) '*'));
+
+    }
+    
+}
